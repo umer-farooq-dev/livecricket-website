61d9490a935ede3c164fe92aa9793e0682cf7eff
umer-farooq-dev <ufkeyfans786@gmail.com>
2022-10-23 18:51:41 +0500

    Updated code

--- cricket/models.py
+++ cricket/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    user = models.TextField(default=None)
+
+    def __str__(self):
+        return self.user
--- cricket/views.py
+++ cricket/views.py
@@ -1,10 +1,32 @@
+from .models import User
 from django.shortcuts import render
+from django.db.models import Q
 
-
-#
 
 def home(request):
-    return render(request=request, template_name='base.html')
+    def get_ip(request):
+        address = request.META.get('HTTP_X_FORWARDED_FOR')
+        if address:
+            ip = address.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    ip = get_ip(request)
+    u = User(user=ip)
+    print(ip)
+    result = User.objects.filter(Q(user__icontains=ip))
+    if len(result) == 1:
+        print("user exist")
+    elif len(result) > 1:
+        print("user exist more .....")
+    else:
+        u.save()
+        print("user is unique")
+    count = User.objects.all().count()
+    print("Total user is ", count)
+
+    return render(request, 'base.html', {'count': count})
 
 
 def privacy_policy(request):
